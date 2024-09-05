d2af9283d2f7f8f3fe99d14d695e1cc05d671564
Sohail-9 <s.suhail9849@gmail.com>
2024-09-05 20:36:03 +0530

    new commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,17 +17,12 @@ const Home = () => {
         <div className="flex flex-col justify-center h-full">
 
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Software Development Engineer.
           </h2>
 
           <p className="text-gray-500 py-4 max-w-md">
-<<<<<<< HEAD
             I have 1 Year of experience building and designing software applications.
             Currently, I'm working on AI, Devops, Full stack and my tech stack is React,js, Next.js, Express.js and TypeScript, Generative AI, Agentic AI.
-=======
-            I have 2 Years of experience building and designing software applications.
-            Currently, I'm working on web applications and my tech stack is React,js, Next.js, Express.js, TypeScript.
->>>>>>> c3c2f6d3c4a7332549e9b29724574e0e7926a799
           </p>
 
 
